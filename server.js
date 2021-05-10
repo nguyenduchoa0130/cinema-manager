9ba5cca43c27724e27766979dbd32dbde8c6d39b
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-10 09:39:36 +0700

    Mound error handler

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const methodOverride = require('method-override');
 const ejsLayouts = require('express-ejs-layouts');
 const routes = require('./routes/index');
 const appMid = require('./middlewares/app.middleware');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
@@ -17,8 +19,9 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(ejsLayouts);
 
-appMid.index(app);// Mount Middlewares
-routes(app);// Mount Routes
+appMid.index(app); // Mount Middlewares
+routes(app); // Mount Routes
+app.use(errorHandler); // Mount Error Handlers
 
 app.listen(PORT, () => {
     console.log('Server is running at ' + PORT);
