8a812b7dd50f3f0cd39b992e061c6759523cbe21
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-11 16:59:19 +0700

    Install: cors, jwt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejs = require('ejs');
 const methodOverride = require('method-override');
 const ejsLayouts = require('express-ejs-layouts');
+const cors = require('cors');
 const routes = require('./routes/index');
 const appMid = require('./middlewares/app.middleware');
 const errorHandler = require('./middlewares/errorHandler');
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
+app.use(cors());// accept all origin
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
