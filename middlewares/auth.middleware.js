3ce2809b373540493efdcec2051126ad8264a3f9
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-13 23:51:23 +0700

    Add middlewares: isSignedIn, isNotSignedIn, isAdmin, isOwnerOrAdmin, authenticate

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,26 +1,44 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 class AuthenticationMiddleware {
-    isSignedIn(req, res, next) {
-        if (req.user) {
+    isNotSignedIn(req, res, next) {
+        if (!req.isAuthenticated()) {
+            return next();
+        } else {
             return res.send('Ban da dang nhap');
         }
-        return next();
+    }
+    isSignedIn(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        } else {
+            return res.send('Ban chua dang nhap');
+        }
+    }
+    isAdmin(req, res, next) {
+        if (req.dataToken.isAdmin) {
+            return next();
+        } else return res.sendStatus(401);
+    }
+    isOwnerOrAdmin(req, res, next) {
+        if (req.dataToken.userId == req.params.id || req.dataToken.isAdmin) {
+            return next();
+        } else return res.sendStatus(401);
     }
     authenticate(req, res, next) {
         try {
             let token = req.headers['authorization']?.split(' ')[1];
             if (!token) {
-                return res.status(401).json({ error: 'Not authorized' });
+                return res.sendStatus(401);
             }
             jwt.verify(
                 token,
                 process.env.ACCESS_TOKEN_SERCET || 'accessToken',
                 (err, data) => {
                     if (err) {
-                        return res.status(401).json({ error: err });
+                        return res.sendStatus(401);
                     } else {
-                        req.data = data;
+                        req.dataToken = data;
                         return next();
                     }
                 }
