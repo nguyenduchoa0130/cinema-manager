14e96842670fbef08a5286a1930019e8592035cd
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-14 13:45:36 +0700

    Fix bug: optional parameter id?

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
-        static associate(models) {}
+        static associate(models) {
+            this.belongsTo(models.Role, {
+                foreignKey: 'roleId',
+            });
+        }
     }
     User.init(
         {
--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userCon = require('../../controllers/user.controller');
 const authMid = require('../../middlewares/auth.middleware');
 router
-    .route('/:id?')
+    .route('/:id')
     .get(userCon.fetchByIdOrEmail)
     .put(authMid.authenticate, authMid.isOwnerOrAdmin, userCon.isIdValid, userCon.update)
     .delete(authMid.authenticate, authMid.isAdmin, userCon.isIdValid, userCon.delete);
