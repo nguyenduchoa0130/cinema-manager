d61ce50b7d5ce8fa4cad04f13368f8b6f9846098
Nguyen Minh Hieu <nmhieu03032000@gmail.com>
2021-05-18 10:45:10 +0700

    thêm dữ liệu hình ảnh

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,72 @@
-console.log(1);
+import React, { Component } from "react";
+import {
+MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class NavbarPage extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  return (
+    <Router>
+      <MDBNavbar color="indigo" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Navbar</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+        <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to="/">Trang chủ</MDBNavLink>
+            </MDBNavItem>
+            
+            <MDBNavItem>
+              <MDBNavLink to="/dang-nhap">Đăng nhập</MDBNavLink>
+            </MDBNavItem>
+
+            <MDBNavItem>
+              <MDBNavLink to="/dang-ky">Đăng ký</MDBNavLink>
+            </MDBNavItem>
+
+            <MDBNavItem>
+              <MDBNavLink to="/trang-ca-nhan">Thông tin cá nhân</MDBNavLink>
+            </MDBNavItem>
+            
+            <MDBNavItem>
+              <MDBNavLink to="/quen-mat-khau">Quên mật khẩu</MDBNavLink>
+            </MDBNavItem>
+
+            <MDBNavItem>
+              <MDBNavLink to="/doi-mat-khau">Đổi mật khẩu</MDBNavLink>
+            </MDBNavItem>
+            
+            <MDBNavItem>
+              <MDBNavLink to="/kich-hoat">OTP</MDBNavLink>
+            </MDBNavItem>
+           </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBFormInline waves>
+                <div className="md-form my-0">
+                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                </div>
+              </MDBFormInline>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        
+        </MDBCollapse>
+      </MDBNavbar>
+    </Router>
+    );
+  }
+}
+
+export default NavbarPage;
