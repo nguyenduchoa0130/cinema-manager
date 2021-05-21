06afdee6f75d37cc07f40c6d7cf9206e398140f5
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-21 19:33:37 +0700

    fix error auth

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 function errorHandler(err, req, res, next) {
     res.status(err.status || 500);
-    res.json({
+    return res.json({
         response: {
             msg: err.message || 'Có 1 lỗi đã xảy ra với server khi chạy request này. Vui lòng liên hệ admin',
             type: err.type || 'Lỗi server',
