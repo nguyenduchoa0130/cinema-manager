64f96b0d9d27c934806a0fbd870a6a5afe4cdad5
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-05-23 21:08:54 +0700

    Redefine error handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,7 @@
 function errorHandler(err, req, res, next) {
     res.status(err.status || 500);
     return res.json({
-        response: {
-            msg: err.message || 'Có 1 lỗi đã xảy ra với server khi chạy request này. Vui lòng liên hệ admin',
-            type: err.type || 'Lỗi server',
-        },
+        msg: err.message || 'Có 1 lỗi đã xảy ra với server khi chạy request này. Vui lòng liên hệ admin',
     });
 }
 module.exports = errorHandler;
