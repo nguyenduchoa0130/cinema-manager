7e2c7f37d8f521713f00c2223bd61c9e62e16336
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-06-03 20:57:48 +0700

    fix bug load logo

--- routes/img.routes.js
+++ routes/img.routes.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const imgCon = require('../controllers/img.controller');
 router.get('/poster/:id', imgCon.poster);
 router.get('/thumb/:id', imgCon.thumbnail);
+router.get('/logo/:id', imgCon.logo);
 module.exports = router;
