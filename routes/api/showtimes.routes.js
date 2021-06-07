b5da8004443c4bb84edc0befdf3a400e15712432
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-06-07 20:31:47 +0700

    fix bug showtimes

--- routes/api/showtimes.routes.js
+++ routes/api/showtimes.routes.js
@@ -3,6 +3,6 @@ const authMid = require('../../middlewares/auth.middleware');
 const showtimeMid = require('../../middlewares/showtimes.middleware');
 const showtimesCon = require('../../controllers/showtimes.controller');
 router.route('/:id').put(showtimesCon.update).delete(showtimesCon.delete);
-router.post('/add', showtimeMid.isValidInfoCinemaManager, showtimeMid.isFilmIdValid, showtimeMid.isValidTimeStart, showtimesCon.insert);
+router.post('/add', showtimeMid.isValidTimeStart, showtimesCon.insert);
 // router.get('/', shiftCon.fetchByCinemaId);
 module.exports = router;
