3271a7c8176ae1054c5d1622f47774f7ecf52bb5
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2021-07-12 21:43:30 +0700

    test

--- middlewares/booking.middleware.js
+++ middlewares/booking.middleware.js
@@ -99,8 +99,8 @@ class BookingMiddleware {
                     },
                 ],
             });
+			return res.json(data);
             if (data.length) {
-				return res.json(data);
                 return next(apiError.badRequest('Không thể xóa suất chiếu. Vì suất chiếu đang có khách hàng đặt vé'));
             } else {
                 return next();
